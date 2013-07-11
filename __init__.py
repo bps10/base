4e01f511d3866d486e42495857888c3505aac6d5
bps10 <brian.patrick.schmidt@gmail.com>
2013-07-11 16:41:17 -0700

    update stockman.py

---
+++ __init__.py
@@ -0,0 +1,6 @@
+import data
+import files
+import image
+import optics
+import plot
+import spectsens
--- spectsens/stockman.py
+++ spectsens/stockman.py
@@ -2,31 +2,37 @@ from __future__ import division
 import numpy as np
 
 
-def stockman(minLambda=390, maxLambda=770, LOG=False,
-				RETURN_SPECTRUM=False, resolution=1):
+def stockman(minLambda=390, maxLambda=770, LOG=False, OpticalDensity=0.5,
+				RETURN_SPECTRUM=False, resolution=1, EXTINCTION=False):
     '''
     '''
     dsamp = resolution * 10.0
-    sens = np.genfromtxt('base/spectsens/stockman/specSens.csv', 
+    exTemp = np.genfromtxt('base/spectsens/stockman/specSens.csv', 
                     delimiter=',')[::dsamp, :]
 
-    spectrum = sens[:, 0]
-    print maxLambda
+    spectrum = exTemp[:, 0]
     # find location of min and max Lambda
     ind1 = 0 #np.where(spectrum == minLambda)[0]
     ind2 = np.where(spectrum == maxLambda)[0]
-    print ind1, ind2
+
     # take only between min and max lambda
     spectrum = spectrum[ind1:ind2 + 1]
-    sens = sens[ind1:ind2 + 1, :]
 
-    if not LOG:
-        sens = 10.0 ** sens
+    exTemp = exTemp[ind1:ind2 + 1, 1:]
+    ODTemp = np.log10((1.0 - 10.0 ** -((10.0 ** exTemp) *
+                        OpticalDensity)) / (1.0 - 10 ** -OpticalDensity))
+    sens = ODTemp
 
-    if RETURN_SPECTRUM:
-        return sens, spectrum
+    if LOG:
+        extinction = exTemp
+        withOD = ODTemp
+    else:
+        extinction = 10.0 ** exTemp
+        withOD = 10.0 ** ODTemp
 
-    return sens
+    if EXTINCTION:
+        return extinction
+    return withOD
 
 def stockmanfund(minLambda=390, maxLambda=770, LOG=False,
 				RETURN_SPECTRUM=False, resolution=1):
@@ -43,7 +49,7 @@ def stockmanfund(minLambda=390, maxLambda=770, LOG=False,
 
     # take only between min and max lambda
     spectrum = spectrum[ind1:ind2 + 1]
-    sens = sens[ind1:ind2 + 1, :]
+    sens = sens[ind1:ind2 + 1, 1:]
 
     if not LOG:
         sens = 10.0 ** sens
