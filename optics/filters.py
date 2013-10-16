6659911c6678a263bdbba01d044fee139b8fada0
bps10 <brian.patrick.schmidt@gmail.com>
2013-10-15 17:37:40 -0700

    added pokorny 1987 age correction for lens

--- optics/filters.py
+++ optics/filters.py
@@ -13,11 +13,11 @@ def stockman(minLambda=390, maxLambda=770, ONLY_LENS=False,
 
     spectrum = lens[:, 0]
     # find location of maxLambda
-    ind1 = 0 # np.where(spectrum == minLambda)[0]
+    ind1 = np.where(spectrum == minLambda)[0]
     ind2 = np.where(spectrum == maxLambda)[0]
 
     spectrum = spectrum[ind1:ind2 + 1]
-    filters = (10.0 ** (lens[ind1:ind2 + 1, 1] + 
+    filters = (10.0 ** (lens[ind1:ind2 + 1, 1] +
         macula[ind1:ind2 + 1, 1]))
 
     if ONLY_LENS:
@@ -27,3 +27,32 @@ def stockman(minLambda=390, maxLambda=770, ONLY_LENS=False,
     if RETURN_SPECTRUM:
         return filters, spectrum
     return filters
+
+
+def lens_age_correction(age, spectrum):
+    '''
+    '''
+    #from scipy import interpolate
+
+    dat = np.genfromtxt('base/optics/pokorny/lens_age_1987.csv', 
+        delimiter=',', names=True)
+    ind = np.where(spectrum == 650)[0] + 1
+
+    # interpolate values from pokorny with spline.
+    # From http://docs.scipy.org/doc/scipy/reference/\
+    # tutorial/interpolate.html#spline-interpolation
+    #f1 = interpolate.interp1d(dat['wavelength'], dat['TL1'])
+    #f2 = interpolate.interp1d(dat['wavelength'], dat['TL2'])
+    TL1 = np.interp(spectrum[:ind], dat['wavelength'], dat['TL1'])
+    TL2 = np.interp(spectrum[:ind], dat['wavelength'], dat['TL2'])
+
+    if age <= 60:
+        lens = TL1 * (1 + 0.02 * (age - 32)) + TL2
+
+    if age > 60:
+        lens = TL1 * (1.56 + 0.0667 * (age - 60)) + TL2
+
+    out = np.zeros(len(spectrum))
+    out[:ind] = lens
+    np.savetxt('test.csv', out, delimiter=',')
+    return out
