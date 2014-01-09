942ddf0d29d4b2bda2db1fc79e55d6ed55c919b0
bps10 <brian.patrick.schmidt@gmail.com>
2014-01-09 15:32:45 -0800

    add try state to import Image

--- data/data.py
+++ data/data.py
@@ -1,6 +1,10 @@
 from __future__ import division
 import numpy as np
-import Image, cPickle
+try:
+    import Image
+except ImportError:
+    from PIL import Image
+import cPickle
 
 ### Data manipulation tools ###
 ###############################
